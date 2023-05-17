31229a8d026dff6ce9e4ff5d8db20e29bfc13fd7
Que Mnqika <qaqambilemnqika23@gmail.com>
2023-05-17 09:24:50 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,21 @@
+let me = "Que";
+console.log(typeof(me));
+
+//reassigning variable
+me = 23;
+console.log(typeof(me));
+
+//variable with all data types
+
+let variables = ['Que', 25, true, null, undefined, arr=new Array(), function(params) {
+    myFunction(0)
+}];
+
+console.log(typeof(variables));
+console.log(typeof(variables[0]));
+console.log(typeof(variables[1]));
+console.log(typeof(variables[2]));
+console.log(typeof(variables[3]));
+console.log(typeof(variables[4]));
+console.log(typeof(variables[5]));
+console.log(typeof(variables[6]));
